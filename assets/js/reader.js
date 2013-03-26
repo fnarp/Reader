eeb9a66c9816beabc803f22993181b056f7ae489
Ash Kyd <ash@kyd.com.au>
2013-03-26 16:18:37 +1000

    Removed logging & added documentation.

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -24,16 +24,29 @@ $(document).ready(function(){
 		return false;
 	});
 	
-	var pushState = function(state, title, url){
+	/**
+	 * pushState, a semishim for history.pushState
+	 * @param state Some state data for this uri. In our case, the AJAX response.
+	 * @param The page title.
+	 * @parem The URN (Bit after the url.)
+	 */
+	var pushState = function(state, title, urn){
 		if(typeof history.pushState != 'undefined'){
-			history.pushState(state,title,url);
+			history.pushState(state,title,urn);
 		} else {
 			// Just a little old browser indifference.
 			// Don't worry about it.
 		}
 	}
 	
-	var showState = function(url,content,modifyHistory){
+	/**
+	 * showState
+	 * Show a page state, commonly used when the AJAX request returns.
+	 * @param urn The URN for this content.
+	 * @param content string containing the AJAX response, commonly the full page. 
+	 * @param modifyHistory Whether to modify the history entry (pushState) when you do.
+	 */
+	var showState = function(urn,content,modifyHistory){
 		var sidebarpos = $('.sidebar .scroller').scrollTop();
 		var title = $(content).find('title').text();
 		var maincontent = $(content).find('.main-content');
@@ -43,7 +56,7 @@ $(document).ready(function(){
 		$('body').scrollTop(0);
 		
 		if(modifyHistory){
-			pushState(content,title,url);
+			pushState(content,title,urn);
 		}
 	}
 	
@@ -68,10 +81,12 @@ $(document).ready(function(){
 		e.preventDefault();
 	});
 	
+	/**
+	 * On pop state.
+	 * This is fired when the back button is hit.
+	 */
 	window.onpopstate = function(e){
-		console.log('state popped');
 		showState(document.location,e.state,false);
-		console.log(e);
 	};
 	
 	
